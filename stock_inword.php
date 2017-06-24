d66988cd6bb59734522f5c9a81be61340e89c685
anilgurjar <anilgurjer371@gmail.com>
2017-06-24 12:28:08 +0530

    sdfg

--- stock_inword.php
+++ stock_inword.php
@@ -4,36 +4,23 @@
  
 	if(isset($_POST['sub']))	
 	{
-		$item_name=$_POST['item_name'];	
-		$price=$_POST['price'];	
-		mysql_query("insert into `master_item` SET `item_name`='$item_name',`price`='$price'");	
+		
+		$item_id=$_POST['item_id'];	
+		$quantity=$_POST['quantity'];	
+		$date=$_POST['date'];	
+		$total=$_POST['total'];	
+		$remark=$_POST['remark'];	
+		$r=mysql_query("insert into `stock_inword` SET `item_id`='$item_id',`quantity`='$quantity',`date`='$date',`total`='$total',`remark`='$remark'");	
+		$r=mysql_query($r);
+		
 	}
 ?>
-<?php 
-	if(isset($_POST['sub_del']))
-	{
-		$delet_item=$_POST['delet_item'];
-		mysql_query("update `master_item` SET `flag`='1' where id='$delet_item'" );
-	}
-	if(isset($_POST['sub_edit']))
-	{
-		$edit=$_REQUEST['edit_id'];  
-		$item_name=mysql_real_escape_string($_REQUEST["item_name"]);
-		$price=mysql_real_escape_string($_REQUEST["price"]);
-		$r=mysql_query("update `master_item` SET `item_name`='$item_name',`price`='$price' where id='$edit'" );
-		$r=mysql_query($r);
-		echo '<script text="javascript">alert(Item Added Successfully")</script>';	
-	}
-	else
-	{
-		echo mysql_error();
-	}  
-?> 
+
 <html>
 	<head>
 		<?php css();?>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Master Item</title>
+		<title>Stock Inword</title>
 	</head>
 	<?php contant_start(); menu();  ?>
 	<body>
@@ -44,7 +31,7 @@
 						<div class="portlet box">
 							<div class="portlet-title">
 								<div class="caption">
-									<i class="fa fa-gift"></i>Master Item
+									<i class="fa fa-gift"></i>Stock Inword
 								</div>
 							</div>
 							<div class="portlet-body form">
@@ -76,7 +63,7 @@
 											<div class="col-md-6">
 												<div class="input-icon right">
 												<i class="fa"></i>
-												<input class="form-control" placeholder="Please Enter quantity" required name="quantity" autocomplete="off" type="text">
+												<input class="form-control" placeholder="Please Enter quantity" required name="quantity" autocomplete="off" id='quantity'  onkeyup="myFunction()" type="text">
 												</div>
 											</div>
 										</div>
@@ -85,6 +72,22 @@
 											<div class="col-md-6">
 												<div class="input-icon right">
 													<input  class="form-control" required="required" placeholder="price" id='item_rate' value="" readonly>	
+												</div>
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="control-label col-md-3">Total</label>
+											<div class="col-md-6">
+												<div class="input-icon right">
+													<input  class="form-control" required="required" placeholder="Total" id='total' value="" name="total" readonly>	
+												</div>
+											</div>
+										</div>
+										<div class="form-group">
+											<label class="control-label col-md-3">Date</label>
+											<div class="col-md-6">
+												<div class="input-icon right">
+													<input  type="date" class="form-control" required="required" placeholder="select date"  name="date" value="date-picker($today)" >	
 												</div>
 											</div>
 										</div>
@@ -124,8 +127,24 @@ $(document).ready(function() {
 	$('.class_id').change(function(){	
 		var amount = $(this).find('option:selected').attr('amount');
 		$('#item_rate').val(amount);
+		$('#total').val(amount);
+		
 	});
+
+		
 });
+function myFunction() 
+	{
+		
+	var x = document.getElementById("quantity");
+    var y = document.getElementById("item_rate");
+    var z = document.getElementById("total");
+	z.value = parseInt(x.value * y.value);
+
+	}
+	
+
+
 </script>
 
 
