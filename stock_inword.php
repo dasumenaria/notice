44019069687687a56ff2e69b3ffc726dba3c2c31
anilgurjar <anilgurjer371@gmail.com>
2017-06-24 09:50:59 +0530

    dfghjk

--- stock_inword.php
+++ stock_inword.php
@@ -40,9 +40,7 @@
 		<div class="page-content-wrapper">
 			<div class="page-content">
 				<div class="row">
-					<div class="col-md-3">
-					</div>
-					<div class="col-md-6">
+					<div class="col-md-12">
 						<div class="portlet box">
 							<div class="portlet-title">
 								<div class="caption">
@@ -56,10 +54,21 @@
 										<div class="form-group">
 											<label class="control-label col-md-3">Item Name</label>
 											<div class="col-md-6">
-												<div class="input-icon right">
-													<i class="fa"></i>
-													<input class="form-control" placeholder="Please Enter item Name" required name="item_name" autocomplete="off" type="text">
-												</div>
+												<i class="fa"></i>
+												<select name="class_id" class="form-control class_id select2me" required="required" placeholder="Select..." id="sname">
+													<option value=""></option>
+															<?php
+																$r1=mysql_query("select `item_name`,`id`,`price` from master_item where   flag = 0  order by id ASC");		
+																$i=0;
+																while($row1=mysql_fetch_array($r1))
+																{
+																$price = $row1['price'];	
+																$id=$row1['id'];
+																$item_name=$row1['item_name'];
+															?>
+													<option value="<?php echo $id; ?>" amount = '<?php echo $price; ?>' ><?php echo $item_name;?></option>
+														<?php } ?> 
+												</select>
 											</div>
 										</div>
 										<div class="form-group">
@@ -75,8 +84,7 @@
 											<label class="control-label col-md-3">Item Rate</label>
 											<div class="col-md-6">
 												<div class="input-icon right">
-													<i class="fa"></i>
-													<input class="form-control" placeholder="Please Enter item Rate" required name="rate" autocomplete="off" type="text">
+													<input  class="form-control" required="required" placeholder="price" id='item_rate' value="" readonly>	
 												</div>
 											</div>
 										</div>
@@ -107,6 +115,19 @@
 <?php footer(); ?>
 
 <?php scripts();?>
+
+
+<script src="assets/global/plugins/jquery.min.js" type="text/javascript"></script>
+<script>
+
+$(document).ready(function() {
+	$('.class_id').change(function(){	
+		var amount = $(this).find('option:selected').attr('amount');
+		$('#item_rate').val(amount);
+	});
+});
+</script>
+
 
 </html>
  
